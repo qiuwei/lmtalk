9223c5d5825029a8365758ecdf0ae7b91bb42356
weiqiu <wei@qiu.es>
2013-02-19 15:41:21 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lmtalk "0.1.0-SNAPSHOT"
+  :description "Language model talk: fed words in, language model generates sentences"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/lmtalk/core_test.clj
@@ -0,0 +1,7 @@
+(ns lmtalk.core-test
+  (:use clojure.test
+        lmtalk.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
